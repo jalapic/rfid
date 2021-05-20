e911e44ec41527df80e7306bc4aaf9ff8ca6c9fd
James Curley <curleyjp0@gmail.com>
2021-05-20 15:00:01 -0500

    2mice

--- R/import_data_pilot.R
+++ R/import_data_pilot.R
@@ -1,7 +1,19 @@
 
 library(tidyverse)
+
+## One mouse pilot
+
 df <- read_csv2("raw_data/rawdata20210519.csv")
 df$data <- as.character(df$data)
 
 df[1:10,]
 table(df$data)
+
+
+## Two mice pilot
+
+dfx <- read_csv2("raw_data/rawdata2mice20210519.csv")
+dfx$data <- as.character(dfx$data)
+
+dfx[1:10,]
+table(dfx$data)
