afaa9be765624018962fc0bcac40a5a49d84c0b6
jalapic <curley@utexas.edu>
2021-06-14 14:47:04 -0500

    new data script

--- R/import_data_pilot.R
+++ R/import_data_pilot.R
@@ -1,16 +1,16 @@
 
 library(tidyverse)
 
-## One mouse pilot
 
-df <- read_csv2("raw_data/rawdata20210519.csv")
+
+## Four mouse pilot
+
+df <- read_csv2("raw_data/Tracking/rawdata20210604.csv")
 df$data <- as.character(df$data)
 
 df[1:10,]
 table(df$data)
 
-
-## Two mice pilot
 
 # cantimestamp = time since run has started
 # datetimestamp = time and data of activity
@@ -18,23 +18,55 @@ table(df$data)
 # antennaID= what number in that channel
 # data = microchip tag
 
-dfx <- read_csv2("raw_data/rawdata2mice20210519.csv")
-dfx$data <- as.character(dfx$data)
-
-dfx[1:10,]
-table(dfx$data)
-
-x <- strptime(as.character(dfx$datetimestamp),'%d.%m.%Y %H:%M:%S:%OS')
+x <- strptime(as.character(df$datetimestamp),'%d.%m.%Y %H:%M:%S:%OS')
 x[[44]]
 
-table(dfx$deviceid, dfx$antennaID)
+table(df$deviceid, df$antennaID)
 
 
 # just look at one antenna/device....
-dfx %>% filter(deviceid==17, antennaID==1) %>% as.data.frame()
+df %>% filter(deviceid==17, antennaID==1) %>% as.data.frame()
 
 
 # filter this mouse....
-dfx %>% filter(data==900133000459724 | data==900133000459725) %>% as.data.frame()
+df %>% filter(data==900133000459723 | data==900133000459727) %>% as.data.frame()
 
+
+# create a column with milliseconds
+range(df$cantimestamp)
+df$time <- df$cantimestamp - min(df$cantimestamp)
+df[1:10,]
+
+plot(df$cantimestamp, type="l")
+
+# date objects are in x
+x[[44]]
+
+lubridate::milliseconds(x[[44]])
+
+
+plot(df$datetimestamp) #won't work - 
+
+#15:21 --> 15:27
+df[525:545,]
+
+df %>% 
+  #filter(deviceid==19) %>%
+  filter(data==900133000459722  | data== 900133000459721) %>%
+  as.data.frame()
+
+
+# assume these belong to same animal....
+mouse1 <- df %>% 
+  #filter(deviceid==19) %>%
+  filter(data==900133000459722  | data== 900133000459721) %>%
+  as.data.frame()
+
+z <- mouse1$deviceid
+z
+rle(z)$values
+
+check_seq <- function(n1,n2,df,id1,id2){
+  code that checks the sequence here
+}
 
