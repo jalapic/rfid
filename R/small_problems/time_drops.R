7713fe536daba3a40053b3932b71bc348954d731
jalapic <curley@utexas.edu>
2021-07-01 11:15:43 -0500

    small problems

---
+++ R/small_problems/time_drops.R
@@ -0,0 +1,11 @@
+####  Imagine we have the following numbers in a sequence
+
+ctime <- c(1.1,1.3,1.6,2.1,2.5,2.7,1.0,1.1,1.4,1.8,1.9,2.1,2.3,2.31,2.5,2.7,1.1,1.4,1.6)
+ms <- c(12,15,16,17,18,19,25,26,27,30,33,36,36,37,39,40,41,43,45)
+
+ctime
+plot(ctime, type='l')
+
+which(lead(ctime)<ctime) + 1 
+
+ms[which(lead(ctime)<ctime) + 1 ]
