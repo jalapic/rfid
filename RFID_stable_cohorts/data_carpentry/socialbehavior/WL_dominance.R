5d955715d13f6eeee70bfb7e6ed797a58c9402b7
ty14 <milewski@utexas.edu>
2021-11-12 14:37:01 -0600

    social behavior analyzed

---
+++ RFID_stable_cohorts/data_carpentry/socialbehavior/WL_dominance.R
@@ -0,0 +1,217 @@
+## just get clean win /loss data 
+library(compete)
+library(PlayerRatings)
+library(tidyverse)
+
+## Read in Data
+
+temp <- list.files(path="RFID_stable_cohorts/data_raw/social_behavior/",pattern="*.csv")
+xfiles <- lapply(temp, function(x) read.csv(paste0("RFID_stable_cohorts/data_raw/social_behavior/",x)) )                 
+lapply(xfiles, head)
+lapply(xfiles, colnames)
+
+lapply(xfiles, head)
+
+lapply(xfiles, tail)
+
+
+# Read in behaviors file
+behavs <- read.csv("RFID_stable_cohorts/data_carpentry/socialbehavior/behavs.csv")
+head(behavs)
+
+
+# Check have all behaviors
+
+## Get all Behavioral Combinations
+behavsX <-
+  unique(
+    unlist(
+      lapply(xfiles, 
+             function(x) 
+               paste(x$Animal.1..Behavior, x$Animal.2..Behavior, sep=" --- "))
+    )
+  )
+
+
+
+behavsX %in% behavs$behavior # if have any FALSE need to update 'behavs' file.
+
+# ##code for updating behaviors
+# 
+# newbehavs <- behavsX[!(behavsX %in% behavs$behavior)]
+# newbehavs<- newbehavs[order(newbehavs)]
+# newbehavs <- as.data.frame(newbehavs)
+# colnames(newbehavs)<-"behavior"
+# write.csv(newbehavs, "RFID_stable_cohorts/data_carpentry/socialbehavior/newbehavs.csv", row.names=F)
+
+# Add in score column.... X = scores in behavs 
+add_score <- function(df){
+  df$behav <-  paste(df$Animal.1..Behavior, df$Animal.2..Behavior, sep=" --- ")
+  df$score <- behavs$X[match(df$behav, behavs$behavior)]
+  return(df)
+}
+
+
+
+xfiles <- lapply(xfiles, add_score)
+lapply(xfiles, head)
+
+
+
+# step 1.  Get Timestamp correct.
+# note some datasets have seconds, others do not.
+get_times <- function(dataf){
+  dataf$ts <- strptime(as.character(dataf$Timestamp),'%m/%d/%Y %H:%M:%S')
+  dataf <- dataf[order(dataf$ts),]
+  return(dataf)
+}
+
+xfiles <- lapply(xfiles, get_times)
+lapply(xfiles, head)
+
+
+# Remove Starts,Ends,NA, when id1==id2  # Remove Starts,Ends,NA, when id1==id2
+clean_data <- function(df){
+  df <- df[!is.na(df$score),]
+  df <- df[df$Start.End!="Start",]
+  df <- df[df$Start.End!="End",]
+  df <- df[df$Animal.1!=df$Animal.2,]
+}
+
+xfiles <- lapply(xfiles, clean_data)
+
+lapply(xfiles, head)
+
+# get winner/loser
+get_wl <- function(df){
+  df$winner <- ifelse(df$score==0, df$Animal.2, df$Animal.1)
+  df$loser <- ifelse(df$score==0, df$Animal.1, df$Animal.2)
+  return(df)
+}
+
+xfiles <- lapply(xfiles, get_wl)
+lapply(xfiles, head)
+
+
+# only keep time, cage, winner, loser, score
+lapply(xfiles, colnames)
+xfiles1 <- lapply(xfiles, function(x) x[,c('ts','Cage','winner','loser','score')])
+lapply(xfiles1, head)
+lapply(xfiles1, tail)
+
+
+# Fix score
+# doing in loop as doesn't seem to work with lapply, don't know why
+## I am not sure what this does ?
+for(i in 1:length(xfiles1)){
+  xfiles1[[i]]$score <- ifelse(xfiles1[[i]]$score==0.5, 0.5, 1)
+}
+
+head(xfiles1[[1]])
+
+# Make into large data frame
+dfnames <- c(1:4)
+wl_df<-xfiles1 %>% map2_df(.,dfnames, ~mutate(.x, cohort = .y)) #not working 
+
+
+### add time
+
+library(lubridate)
+wl_df<- wl_df %>% 
+  mutate(date = date(wl_df$ts)) %>% 
+  mutate(hrs = hour(wl_df$ts)) %>% 
+  mutate(min = minute(wl_df$ts)) %>% 
+  mutate(sec = second(wl_df$ts)) %>% 
+  select( -ts)  # add dates and times and remove timestamp
+
+
+write.csv(wl_df, "RFID_stable_cohorts/data_clean/socialbehavior/Pre_WL.csv", row.names=F)
+
+
+#David scores
+
+## David's Scores....
+
+get_matrix <- function(df){
+  df <-df[df$score==1,]
+  mat <-compete::org_matrix(compete::get_wl_matrix(df[,c('winner','loser')]),
+                            method = "ds")
+  return(mat)
+}
+
+mats <- lapply(xfiles1, get_matrix)
+dscores <- lapply(mats, compete::ds)
+
+dscores
+
+dsdf <- data.frame(unlist(dscores))
+dsdf$cohort <- substr(rownames(dsdf),1,1)
+dsdf$id <- substr(rownames(dsdf),3,3)
+colnames(dsdf)[1]<-"dscore"
+
+dsdf <- dsdf %>% group_by(cohort) %>% mutate(rank = min_rank(-dscore))
+
+head(dsdf)
+
+
+#WL mats 
+mats
+
+bimat <- lapply(mats, get_di_matrix)
+bimat
+
+matrixplot <- function(m, mylevs=NULL, lowcolor="white",highcolor="red1"){
+  
+  library(ggplot2)
+  
+  #make the df we will use for plotting
+  m.dat <- reshape2::melt(m)
+  m.dat <- data.frame(m.dat)
+  m.dat <- m.dat[complete.cases(m.dat),] #removing NAs
+  
+  if(is.null(mylevs)) { mylevs = rownames(m)}
+  
+  #reorder the levels of the y-axis so plots properly
+  m.dat$loser <- factor(m.dat$loser, levels=mylevs)
+  m.dat$winner <- factor(m.dat$winner, levels = rev(mylevs))
+  m.dat[m.dat == 0] <- NA
+  
+  
+  #plot
+  p1<-ggplot(m.dat, aes(loser, winner, fill = value)) + 
+    geom_tile(colour="black", 
+              size=0.5, stat="identity") + 
+    geom_text(data=m.dat, aes(loser, winner, label = value), color="black", size=rel(3.5)) +
+    scale_fill_gradient(low = lowcolor, high = highcolor, space = "Lab", 
+                        na.value = "white", guide = "colourbar") +
+    scale_x_discrete(expand = c(0, 0), position = "top") +
+    scale_y_discrete(expand = c(0, 0)) +
+    xlab("Loser") + 
+    ylab("Winner") +
+    theme(axis.text.x = element_text(vjust = 1),
+          axis.text.y = element_text(hjust = 0.5),
+          panel.grid.major = element_blank(),
+          panel.grid.minor = element_blank(),
+          panel.border = element_rect(fill=NA,color="black", size=0.5, linetype="solid"),
+          axis.line = element_blank(),
+          axis.ticks = element_blank(),
+          panel.background = element_rect(fill="white"),
+          plot.background = element_rect(fill="white"),
+          axis.text = element_text(color="#3C3C3C", size=rel(1.1)),
+          legend.position = "none"        
+    ) 
+  return(p1)
+}
+
+mat_plots <- lapply(mats,matrixplot)
+egg::ggarrange(plots=mat_plots, widths = c(1,1), labels = c("1", "2", "3", "4"))
+
+
+## dominant stats
+lapply(mats, dc_test)
+
+
+#
+
+
+
