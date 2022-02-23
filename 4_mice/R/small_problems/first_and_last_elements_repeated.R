51ed37c6002feddb7486dacce3fe3ad418db2d7d
fusillomia <mia.fusillo@utexas.edu>
2022-02-23 10:05:07 -0600

    first and last observations — chase code

--- 4_mice/R/small_problems/first_and_last_elements_repeated.R
+++ 4_mice/R/small_problems/first_and_last_elements_repeated.R
@@ -3,8 +3,9 @@
 
 x <- c(3,3,3,3,1,1,1,1,16,1,1,1,16,16,16,16,16,3,3,3,3,9,9,9,9,1,1,1,16,16,1,3,9,9,9)
 
-
+## first and last of a set of repeated observations
 x1 <- sort(unique(c(length(x) - cumsum(rle(rev(x))$lengths) + 1, cumsum(rle(x)$lengths) )))
 x1
 
 x[x1]
+
